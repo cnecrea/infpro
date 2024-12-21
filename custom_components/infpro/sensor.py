a28dcb9253e6b0a4ee4e85930d8636aae02ed0e1
Ciprian Nicolae <48139709+cnecrea@users.noreply.github.com>
2024-12-21 10:08:19 +0200

    Update sensor.py

--- custom_components/infpro/sensor.py
+++ custom_components/infpro/sensor.py
@@ -16,7 +16,6 @@ _LOGGER = logging.getLogger(__name__)
 
 URL = "http://shakemap4.infp.ro/atlas/data/event.pf"
 
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     """Configurează senzorul de cutremur INFP."""
     _LOGGER.debug("Inițiem configurarea senzorului Cutremur România (INFP).")
@@ -31,17 +30,16 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
         async_add_entities([sensor], update_before_add=True)
         _LOGGER.debug("Senzorul a fost adăugat cu succes.")
 
-        # Programează actualizările periodice
-        async_track_time_interval(
-            hass, sensor.async_update, timedelta(seconds=update_interval)
-        )
-        _LOGGER.debug("Am creat track_time_interval cu intervalul %s secunde.", update_interval)
+        # Programează actualizările periodice utilizând coordonatorul dinamic
+        sensor.set_update_interval(update_interval)
+
     except Exception as e:
         _LOGGER.error("A apărut o eroare la configurarea senzorului: %s", str(e))
 
 
 class InfpEarthquakeSensor(Entity):
     """Reprezintă senzorul de cutremur INFP."""
+
     def __init__(self, hass: HomeAssistant, entry: ConfigEntry, update_interval: int):
         self.hass = hass
         self.entry = entry
@@ -49,6 +47,7 @@ class InfpEarthquakeSensor(Entity):
         self._state = None
         self._attributes = {}
         self._name = "Cutremur"
+        self._update_task = None
 
         _LOGGER.debug(
             "Senzorul inițializat cu intervalul de actualizare: %s secunde",
@@ -117,6 +116,19 @@ class InfpEarthquakeSensor(Entity):
             except Exception as e:
                 _LOGGER.error("Eroare la actualizarea datelor: %s", str(e))
                 self._attributes = {"Eroare": str(e)}
+
+    def set_update_interval(self, update_interval: int):
+        """Setează intervalul de actualizare."""
+        if self._update_task:
+            self._update_task()  # Dezactivează sarcina existentă
+
+        self._update_interval = update_interval
+        self._update_task = async_track_time_interval(
+            self.hass, self.async_update, timedelta(seconds=update_interval)
+        )
+        _LOGGER.debug(
+            "Intervalul de actualizare a fost setat la: %s secunde.", update_interval
+        )
 
     @staticmethod
     async def fetch_data(session: aiohttp.ClientSession) -> str:
